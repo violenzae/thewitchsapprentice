c6b0815bed75137a92a39ff38475712ed6ad7580
Rachel Schieferstein <violenza@gmail.com>
2020-04-08 15:47:33 -0700

    adding clickable buttons to pause menu
    Co-authored-by: Mariam Alaeddin <maralaeddin@gmail.com>
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -23,17 +24,28 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Resume ()
+    public void Resume ()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
-    void Pause ()
+    public void Pause ()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("StartButton");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
     }
 }
